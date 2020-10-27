f03be157354514eadd502fe3385122823967b6a7
CUONG PHAM <cuongpd122@gmail.com>
2020-10-27 15:22:22 +0700

    Update Composer

--- helper.php
+++ helper.php
@@ -421,3 +421,40 @@ if (!function_exists('get_number')) {
         return (int) $number;
     }
 }
+
+if (!function_exists('backend')) {
+    function backend($slug = 'home')
+    {
+        $backend_url = BASE_URL . 'backend/';
+        $slug = str_replace('.', '/', $slug);
+        $slug = rtrim($slug, '/');
+        return $backend_url . strtolower(trim($slug));
+    }
+}
+
+if (!function_exists('getKeyName')) {
+    function getKeyName($key = '')
+    {
+        $key = str_replace('_', ' ', $key);
+        $key = ucwords($key);
+        return trim($key);
+    }
+}
+
+if (!function_exists('backend_route')) {
+    function backend_route($controller, $action, $submit = false)
+    {
+    }
+}
+
+if (! function_exists('no_data')) {
+    function no_data($title = '', $desc = '')
+    {
+        $title = $title ? $title : 'Không có dữ liệu';
+        $desc = $desc ? $desc : 'Không tìm thấy dữ liệu trên hệ thống, vui lòng thử lại sau!';
+        return "<div class='no-data-screen-wrap text-center my-2 pb-2'>
+            <h3 class='no-data-title'>{$title}</h3>
+            <h5 class='no-data-subtitle'>{$desc}</h5>
+        </div>";
+    }
+}
